3e9261c698b43eb66377e49092faa70aaaa571a2
tim <timothy.liam.mullen@gmail.com>
2015-11-21 12:01:38 -0500

    view is pretty much fixed

--- settings.py
+++
@@ -1,5 +0,0 @@
-import pyglet
-
-TILE_MAP_IMAGE = pyglet.image.load('f-tilemap.png')
-TILE_MAP = pyglet.image.ImageGrid(TILE_MAP_IMAGE, 26, 8)
-
--- view.py
+++ view.py
@@ -8,21 +8,24 @@ class View(object):
         self.bottom = self.get_from_percent(window.height, bottom_percent)
         self.width  = self.get_from_percent(window.width, width_percent)
         self.height = self.get_from_percent(window.height, height_percent)
-        self.scale_x = window.width / self.width
-        self.scale_y = window.height / self.height
-        self.scale = scale
+        self.scale_x = self.width / scale
+        self.scale_y = self.height / scale
+        print "{0} {1}".format(self.scale_x, self.scale_y)
 
     def get_from_percent(self, window_attr, percent):
         return math.floor(window_attr * (percent / 100.))
 
-    def draw_item(self, image_to_blit, x, y, width, height):
-        x_to_draw = math.floor(self.scale_x * (x + self.left))
-        y_to_draw = math.floor(self.scale_y * (y + self.bottom))
+    def draw_item(self, image_to_blit, x, y, width, height, s=False):
+        x_to_draw = (self.scale_x * x) + self.left
+        y_to_draw = (self.scale_y * y) + self.bottom
         width_to_draw = math.floor(width * self.scale_x)
         height_to_draw = math.floor(height * self.scale_y)
 
         x_in_bound = self.left < x_to_draw < self.left + self.width
         y_in_bound = self.bottom < y_to_draw < self.bottom + self.height
+
+        if s:
+            print "{0} {1} {2} {3}".format(x_to_draw, y_to_draw, width_to_draw, height_to_draw)
 
         image_to_blit.blit(x_to_draw,
                     y_to_draw,
@@ -32,7 +35,7 @@ class View(object):
 class Animator(object):
 
     def __init__(self, window):
-        self.view = View(window, 10, 10, 10, 90, 90)
+        self.view = View(window, 10., 15, 30, 70, 68)
 
     def draw_tiles(self, atlas):
         for col_num, column in enumerate(atlas.atlas):
@@ -42,7 +45,7 @@ class Animator(object):
 
     def draw_hero(self, hero):
         self.view.draw_item(hero.image, hero.x, hero.y,
-                width=1, height=1)
+                width=1, height=1, s=True)
 
     def draw_enemies(self, enemies):
         for enemy in enemies.enemies:
