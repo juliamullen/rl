bac2ef3ec8b07c09ce6a7f7d71c77bba78912c10
tim <timothy.liam.mullen@gmail.com>
2015-11-21 11:41:46 -0500

    next commit fixes the view

--- view.py
+++ view.py
@@ -3,18 +3,19 @@ import math
 TILE_SIZE = 55
 
 class View(object):
-    def __init__(self, window, left_percent, bottom_percent, width_percent, height_percent):
+    def __init__(self, window, scale, left_percent, bottom_percent, width_percent, height_percent):
         self.left   = self.get_from_percent(window.width, left_percent)
         self.bottom = self.get_from_percent(window.height, bottom_percent)
         self.width  = self.get_from_percent(window.width, width_percent)
         self.height = self.get_from_percent(window.height, height_percent)
         self.scale_x = window.width / self.width
         self.scale_y = window.height / self.height
+        self.scale = scale
 
     def get_from_percent(self, window_attr, percent):
         return math.floor(window_attr * (percent / 100.))
 
-    def draw_item(self, image_to_blit, x, y, width, height, s=False):
+    def draw_item(self, image_to_blit, x, y, width, height):
         x_to_draw = math.floor(self.scale_x * (x + self.left))
         y_to_draw = math.floor(self.scale_y * (y + self.bottom))
         width_to_draw = math.floor(width * self.scale_x)
@@ -31,19 +32,19 @@ class View(object):
 class Animator(object):
 
     def __init__(self, window):
-        self.view = View(window, 10, 10, 90, 90)
+        self.view = View(window, 10, 10, 10, 90, 90)
 
     def draw_tiles(self, atlas):
         for col_num, column in enumerate(atlas.atlas):
             for row_num, tile in enumerate(column):
-                self.view.draw_item(tile.image, TILE_SIZE*col_num, TILE_SIZE*row_num,
-                       width=TILE_SIZE, height=TILE_SIZE)
+                self.view.draw_item(tile.image, col_num, row_num,
+                       width=1, height=1)
 
     def draw_hero(self, hero):
-        self.view.draw_item(hero.image, TILE_SIZE*hero.x, TILE_SIZE*hero.y,
-                width=TILE_SIZE, height=TILE_SIZE, s=True)
+        self.view.draw_item(hero.image, hero.x, hero.y,
+                width=1, height=1)
 
     def draw_enemies(self, enemies):
         for enemy in enemies.enemies:
-            self.view.draw_item(enemy.image, TILE_SIZE*enemy.x, TILE_SIZE*enemy.y,
-                    width=TILE_SIZE, height=TILE_SIZE)
+            self.view.draw_item(enemy.image, enemy.x, enemy.y,
+                    width=1, height=1)
